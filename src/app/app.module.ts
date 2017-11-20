6fe2b93bfcb975b10bdcfeeab04494e483574422
anasmah <mahdaoui.anas@gmail.com>
2017-11-20 22:03:19 +0000

    Angular-1

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import {FormsModule} from "@angular/forms"
+import {AboutService} from "../services/about.service";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ContactsComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule
+  ],
+  providers: [AboutService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
